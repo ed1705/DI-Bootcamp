9a15adfe59343e682c76c52a78654e7f6ac4aa97
Errol Gamsu <errolgamsu@Errols-iMac.local>
2022-06-27 17:05:30 +0300

    daily excercisexp

---
+++ Week2/Day1/ExercisesXPGold/script.js
@@ -0,0 +1,287 @@
+// // 🌟 Exercise 1: Your Favorite Food
+// // Instructions
+// // Store your favorite food into a variable.
+
+// // Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
+
+// // Console.log I eat <favorite food> at every <favorite meal>
+
+// let myFavoriteFood = "pizza";
+// let myFavoriteMeal = "lunch";
+// console.log(`I eat ${myFavoriteFood} at every ${myFavoriteMeal}`);
+
+
+// // 🌟 Exercise 2 : Series1
+// // Instructions
+// // Part I
+// // Using this array : let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+// // Create a variable named myWatchedSeriesLength that is equal to the number of series in the myWatchedSeries array.
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+// let myWatchedSeriesLength = [myWatchedSeries.length];
+// console.log(myWatchedSeriesLength);
+
+// // Create a variable named myWatchedSeriesSentence, that is equal to a sentence describing the series you watched
+// let myWatchedSeriesSentence = `${myWatchedSeries[0]} ${myWatchedSeries[1]} and ${myWatchedSeries[2]}`;
+// console.log(myWatchedSeriesSentence);
+
+// // For example : black mirror, money heist, and the big bang theory
+
+// // Console.log a sentence using both of the variables created above
+// // For example : I watched 3 series : black mirror, money heist, and the big bang 
+
+// console.log(`I have not watched all ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}.`);
+
+// // Part II
+// // Change the series “the big bang theory” to “friends”. Hint : You will need to use the index of “the big bang theory” series.
+// myWatchedSeries.splice(2,1, "friends");
+// // Add, at the end of the array, the name of another series you watched.
+// myWatchedSeries.push("Breaking Bad");
+
+// // Add, at the beginning of the array, the name of your favorite series.
+// myWatchedSeries.unshift("Stranger Things");
+
+// // Delete the series “black mirror”.
+// myWatchedSeries.splice(1,1);
+
+// // Console.log the third letter of the series “money heist”.
+// console.log(myWatchedSeries[1][2]);
+// // Finally, console.log the myWatchedSeries array, to see all the modifications you’ve made.
+// console.log(myWatchedSeries);
+
+// // 🌟 Exercise 3 : The Temperature Converter
+// // Instructions
+// // Store a celsius temperature into a variable.
+// // Convert it to fahrenheit and console.log <temperature>°C is <temperature>°F.
+// // Hint : Should you create another variable to hold the temperature in fahrenheit? (ie. point 2)
+// // Hint: To convert a temperature from celsius to fahrenheit : Divide it by 5, then multiply it by 9, then add 32
+// let celsiusTemp = [30];
+// let fahrenheitTemp = [(celsiusTemp/5*9)+32];
+// console.log(`${celsiusTemp}°C is ${fahrenheitTemp}°F`);
+
+
+
+// // 🌟 Exercise 4 : Guess The Answers #1
+// // Instructions
+// // For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// // Of course, explain each prediction.
+// // Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+// // For example
+
+// // console.log(2+3)
+// // // Prediction: It will output 5, because 2 and 3 are numbers
+// // // Actual: 5
+
+
+// // Using the code below:
+
+//     let c;
+//     let a = 34;
+//     let b = 21;
+
+//     console.log(a+b) //first expression
+//     // Prediction: It will output 55, because values of a and b are numbers
+//     // Actual: 55
+
+//     a = 2;
+
+//     console.log(a+b) //second expression
+//     // Prediction: It will output 23, same as above but now a's value has changed to the number 2.
+//     // Actual: 23
+
+// // What will be the outcome of a + b in the first expression ? 
+// 55
+// // What will be the outcome of a + b in the second expression ?
+// 23
+// // What is the value of c?
+// undefined
+// // Analyse the code below, what will be the outcome?
+// console.log(3 + 4 + '5'); 
+// = 75
+
+// // Exercise 5 : Guess The Answers #2
+// // Instructions
+// // For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// // Of course, explain each prediction.
+// // Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+
+// typeof(15)
+// // Prediction: number
+// // Actual:
+
+// typeof(5.5)
+// // Prediction: number
+// // Actual:
+
+// typeof(NaN)
+// // Prediction:undefined 
+// // Actual: NUMBER
+
+// typeof("hello")
+// // Prediction: string
+// // Actual:
+
+// typeof(true) 
+// // Prediction: boolean
+// // Actual:
+
+// typeof(1 != 2)
+// // Prediction: boolean
+// // Actual:
+
+// "hamburger" + "s" 
+// // Prediction: hamburgers
+// // Actual:
+
+// "hamburgers" - "s"
+// // Prediction: NAN 
+// // Actual:
+
+// "1" + "3"
+// // Prediction: 13
+// // Actual:
+
+// "1" - "3"
+// // Prediction: nan
+// // Actual: -2 (????)
+
+// "johnny" + 5 
+// // Prediction: johnny5
+// // Actual:
+
+// "johnny" - 5
+// // Prediction: NAN
+// // Actual:
+
+// 99 * "hello"
+// // Prediction: NAN
+// // Actual:
+
+// 1 != 1
+// // Prediction: False
+// // Actual:
+
+// 1 == "1"
+// // Prediction: False ???? comparing values
+// // Actual: True 
+
+// 1 === "1"
+// // Prediction: false comparing values and data type
+// // Actual:
+
+// // Exercise 6 : Guess The Answers #3
+// // Instructions
+// // For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// // Of course, explain each prediction.
+// // Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+
+
+// // What is the output of each of the expressions below?
+// 5 + "34"
+// // Prediction: 534
+// // Actual:
+
+// 5 - "4"
+// // Prediction: 1
+// // Actual:
+
+// 10 % 5
+// // Prediction:nan
+// // Actual: 0
+
+// 5 % 10
+// // Prediction:
+// // Actual: 5 
+
+// "Java" + "Script"
+// // Prediction: JavaScript
+// // Actual:
+
+// " " + " "
+// // Prediction: ' '
+// // Actual:
+
+// " " + 0
+// // Prediction:
+// // Actual: 
+// ' 0'
+
+// true + true
+// // Prediction: true 
+// // Actual: 2 
+
+// true + false
+// // Prediction: ?
+// // Actual:2
+
+// false + true
+// // Prediction:? 
+// // Actual:1
+
+// false - true
+// // Prediction: ? 
+// // Actual: -1
+
+// !true
+// // Prediction:false
+// // Actual:
+
+// 3 - 4
+// // Prediction: -1
+// // Actual:
+
+// "Bob" - "bill"
+// // Prediction: NAN
+// // Actual:
+
+
+
+// Exercise 1 : Favorite Color
+// Instructions
+// let sentence = ["my","favorite","color","is","blue"];
+// Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+let sentence = ["my","favorite","color","is","blue"];
+console.log(sentence.join());
+
+
+// Exercise 2 : Mixup
+// Instructions
+// Create 2 variables. The values should be strings. For example:
+let str1 = "forest";
+let str2 = "jungle";
+
+
+// 2. Slice out and swap the first 2 characters of the two strings from part 1.
+
+let newstring = str1.splice(0, 2);
+let newstring2 = str2.splice(0, 2)
+console.log(newstring2, newstring2)
+
+
+
+// 3. Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
+
+
+// 4. Finally console.log the new concatenated string.
+
+
+
+// Some Examples
+
+// let str1 = "mix";
+// let str2 = "pod";
+// let newWord should be equal to 'pox mid'
+
+// let firstWord = "Hello";
+// let secondWord = "World";
+// let thirdWord should be equal to 'Wollo Herld'
+
+
+
+
+
+
+
---
+++ Week2/Day2/lessonW2D2/script.js
@@ -0,0 +1,195 @@
+// // Nested arrays
+// let tshirts = [["tshirt1", "M", 12], ["tshirt2", "L", 10]];
+
+// // objects
+
+// // let variableName = {
+// //   property: value,
+// //   property: value,    
+// // };
+
+// // array is an ordered list
+// let colors = ["blue", "red", "yellow"];
+// let favoriteColor = colors[1]
+
+// let tshirt1 = {
+// 	size : "M",
+// 	price: 12,
+// }
+
+// // syntax to retrieve data from an object
+// // dot notation
+// // objectName.keyName
+// // square bracket notation
+// // objectName["keyName"]
+
+// let priceShirt = tshirt1.price;
+// let priceShirtOther = tshirt1["price"];
+
+// // console.log(priceShirt) //12
+// // console.log(priceShirtOther) //12
+// // console.log(tshirt1.price, tshirt1.size)
+
+// // add the color of the shirt
+// tshirt1["color"] = "red";
+// // console.log(tshirt1)
+
+// // modify a value
+// tshirt1["size"] = "L";
+// console.log(tshirt1)
+
+// let family = {
+// 	lastname : "Smith",
+// 	members : 4,
+// 	hasADog : true,
+// 	nameOfMembers : ["Lea", "David", "Mom", "Dad"],
+// 	friends : {
+// 		name : "Jack",
+// 		lastname : "ABC",
+// 		age : 12,
+// 		favoriteColors : ["blue", "red"],
+// 	}
+// };
+
+// //1 Name of the family
+// // console.log(family.lastname);
+// console.log(family["lastname"]);
+
+// //2. name of 2nd member of the family
+// console.log(family["nameOfMembers"][1]);
+
+// //3. Age of the friend
+// console.log(family["friends"]["age"]);
+// //the same as
+// console.log(family.friends.age);
+
+// //4. Display the first favorite color of Jack
+// console.log(family["friends"]["favoriteColors"][0]);
+// // same
+// console.log(family.friends.favoriteColors[0]);
+
+
+// // I want to sell 3 tshirst
+// // each one has a size, a color, a price
+
+// // a list of tshirt, I need an array
+
+// // let shopping = [<1>, <2>, <3>];
+// // an array of object
+// let shopping = [
+// 	{ 
+// 		price: 12,
+// 		size: "M",
+// 		color: "red"
+// 	}, 
+// 	{
+// 		price: 14,
+// 		size: "L",
+// 		color: "blue"
+// 	}, 
+// 	{
+// 		price: 20,
+// 		size: "S",
+// 		color: "yellow"
+// 	}
+// ];
+
+// console.log(shopping)
+// // how to get the price of the 2nd tshirt
+// console.log(shopping[1]["price"])
+// // same
+// console.log(shopping[1].price)
+
+// let list = ["red", "blue"]
+
+
+// // let allT = {
+// // 	tshirt1 : {
+
+// // 	},
+// // }
+
+
+// // 1. Add the lastname Smith to the object
+// // 2. Change the price of the pear, so it will contain the Taxes. (17%)
+// // 3. Ask the user for the fruit he wants between Apple, Banana and Pear.
+// // Make sure that your code accept all type of strings,
+// // for example "Banana" or "banana" or "BaNaNA"
+// // 4. Console.log the price for the specific fruit the user wants
+
+// let userCart = {
+// 	username : "John",
+// 	password: 1234,
+// 	isUserSignedIn : true,
+// 	cart : {
+// 		apple : 2,
+// 		banana : 1,
+// 		pear : 5,
+// 	},
+// 	prices : {
+// 		apple : 0.5,
+// 		banana : 0.8,
+// 		pear : 0.2
+// 	}
+// };
+
+// userCart["lastname"] = "Smith";
+// userCart["prices"]["pear"] = userCart["prices"]["pear"]*1.17
+// // or shorthand
+// userCart["prices"]["pear"] *= 1.17; 
+
+// let userAnswer = prompt("fruit he wants?").toLowerCase();
+// console.log(userCart["prices"][userAnswer])
+
+
+// // console.log(userCart)
+//  let age = Number(prompt("how old are you?"));
+
+// if (age < 18) {
+// 	alert("Sorry, you are too young to drive this car. Powering off")
+// } else if (age == 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!")
+// }
+// else {
+// 	alert("Powering On. Enjoy the ride!")
+// }
+
+
+
+// let userCartLog = {
+// 	username : "Johnny",
+// 	password: 12345,
+// 	isUserSignedIn : true,
+// };
+
+// // PART I
+// // 1. If the user is SignedIn - show him his name and password
+// if (isUserSignedIn = true) {
+// 	console.log((userCartLog["username"]),(userCartLog["password"]))
+// 	} else {
+// 		alert("You need to sign in")
+	// }
+
+// 2. If not - alert the user "you need to sign in"
+// PART II
+// 2nd exercise
+// 1. If the user is Johnny AND his password is 12345 - alert him "You are signed in"
+// 2. If the user is Johnny OR his password is 1234 - alert him "One credential is false"
+// 3. Else,alert the user "you need to sign in
+let userCartLog = {
+	username : "Johnny",
+	password: 1234,
+	isUserSignedIn : true,
+}; 
+if ((userCartLog["username"] == "Johnny") && (userCartLog["password"] == "12345")) {
+	console.log("You are signed in")
+}
+
+else if ((userCartLog["username"] == "Johnny") || (userCartLog["password"] = "1234")) {
+	console.log("One credential is false");
+} else  console.log("you need to sign in!")
+
+
+
+
+
