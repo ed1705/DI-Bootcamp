707a2aa6acaa801f1d30052436219df190e73a98
Errol Gamsu <errolgamsu@Errols-iMac.local>
2022-06-28 10:28:14 +0300

    morning  changes

--- Week2/Day2/ExercisesXP/script.js
+++ Week2/Day2/ExercisesXP/script.js
@@ -84,7 +84,7 @@ if (users.length === 0) {
 	else if (users.length === 2){
 		console.log(`${users[0]} and ${users[1]} is online`);
 	}
-    else if (users.length > 2){
+    else  {
 		console.log(`${users[0]} and ${users[1]} and ${users.length-2} more are online`);
 	}
 
