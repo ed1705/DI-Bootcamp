fd63d7eaa2fe4eb945a9480aca01e4e7497e1033
Errol Gamsu <errolgamsu@Errols-iMac.local>
2022-08-09 20:52:05 +0300

    pushdaily

--- wk8/day2/DailyChallenge/vote/src/Voting.js
+++ wk8/day2/DailyChallenge/vote/src/Voting.js
@@ -1,1 +1,40 @@
-import React, {Component} 
+import React, {Component} from 'react';
+
+class Voting extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      languages : [
+          {name: "Php", votes: 1},
+          {name: "Python", votes: 0},
+          {name: "JavaSript", votes: 0},
+          {name: "Java", votes: 0}
+      ]
+    }
+  }
+
+  handleClick = (lang) => {
+    lang.votes++;
+    console.log(this.state.languages);
+    this.setState({languages:[...this.state.languages]})
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Voting App</h1>
+        {
+          this.state.languages.map( (item,i) =>{
+            return(
+              <div key={i} style={{border:'1px solid #fff',padding:'20px',margin:'10px'}}>
+                {item.name} {item.votes}
+                <button onClick={()=>this.handleClick(item)}>Vote</button>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+export default Voting
